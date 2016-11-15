4e8837f67e912f829272944105cf247e22609b16
a_teammate <madoe3@web.de>
2016-11-15 18:13:30 +0100

    works in vs15
    use it with find_package(Protobuf) in cmake

--- conanfile.py
+++ conanfile.py
@@ -6,85 +6,70 @@ import shutil
 class ProtobufConan(ConanFile):
     name = "Protobuf"
     version = "3.1.0"
-    url = "https://github.com/sunsided/conan-protobuf.git"
-    license = "https://github.com/google/protobuf/blob/v3.1.0/LICENSE"
+    url = "https://github.com/inexor-game/conan-protobuf.git"
+    license = "https://github.com/google/protobuf/blob/v{}/LICENSE".format(version)
     requires = "zlib/1.2.8@lasote/stable"
     settings = "os", "compiler", "build_type", "arch"
-    exports = "CMakeLists.txt", "lib*.cmake", "extract_includes.bat.in", "protoc.cmake", "tests.cmake", "change_dylib_names.sh"
+    exports = "cmake*.cmake"
+   # exports = "CMakeLists.txt", "lib*.cmake", "extract_includes.bat.in", "protoc.cmake", "tests.cmake", "change_dylib_names.sh"
     options = {"shared": [True, False]}
-    default_options = "shared=True"
+    default_options = "shared=False"
     generators = "cmake"
+    folder = "protobuf-{}".format(version)
 
     def config(self):
         self.options["zlib"].shared = self.options.shared
 
     def source(self):
         tools.download("https://github.com/google/protobuf/"
-                       "releases/download/v3.1.0/protobuf-cpp-3.1.0.zip",
+                       "releases/download/v{0}/protobuf-cpp-{0}.zip".format(self.version),
                        "protobuf.zip")
         tools.unzip("protobuf.zip")
         os.unlink("protobuf.zip")
+        cmake_file = "{}/cmake/CMakeLists.txt".format(self.folder)
+        tools.replace_in_file(cmake_file, "project(protobuf C CXX)", '''project(protobuf C CXX)
+include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+conan_basic_setup()''')
+        tools.replace_in_file(cmake_file, "include(install.cmake)", "# include(install.cmake) # commented by conan: we install manually")
 
     def build(self):
-        if self.settings.os == "Windows":
-            args = ['-DBUILD_TESTING=OFF']
-            args += ['-DBUILD_SHARED_LIBS=%s' % ('ON' if self.options.shared else 'OFF')]
-            cmake = CMake(self.settings)
-            self.run('cd protobuf-3.1.0/cmake && cmake . %s %s' % (cmake.command_line, ' '.join(args)))
-            self.run("cd protobuf-3.1.0/cmake && cmake --build . %s" % cmake.build_config)
-        else:
-            env = ConfigureEnvironment(self.deps_cpp_info, self.settings)
-
-            concurrency = 1
-            try:
-                import multiprocessing
-                concurrency = multiprocessing.cpu_count()
-            except (ImportError, NotImplementedError):
-                pass
-
-            self.run("chmod +x protobuf-3.1.0/autogen.sh")
-            self.run("chmod +x protobuf-3.1.0/configure")
-            self.run("cd protobuf-3.1.0 && ./autogen.sh")
-
-            args = []
-            if not self.options.shared:
-                args += ['--disable-shared']
-
-            self.run("cd protobuf-3.1.0 && %s ./configure %s" % (env.command_line, ' '.join(args)))
-            self.run("cd protobuf-3.1.0 && make -j %s" % concurrency)
+        args = ["-Dprotobuf_BUILD_TESTS=OFF", "-Dprotobuf_BUILD_EXAMPLES=OFF"]
+        args += ["-DBUILD_SHARED_LIBS={}".format('ON' if self.options.shared else 'OFF')]
+        args += ["-Dprotobuf_WITH_ZLIB=ON"]
+        if self.settings.compiler == "Visual Studio":
+            if self.settings.compiler.runtime == "MT" or self.settings.compiler.runtime == "MTd":
+                args += ["-Dprotobuf_MSVC_STATIC_RUNTIME=ON"]
+            else:
+                args += ["-Dprotobuf_MSVC_STATIC_RUNTIME=OFF"]
+        cmake = CMake(self.settings)
+        self.run('cmake {}/cmake {} {}'.format(self.folder, cmake.command_line, ' '.join(args)))
+        self.run("cmake --build . {}".format(cmake.build_config))
 
     def package(self):
-        self.copy_headers("*.h", "protobuf-3.1.0/src")
+        # Copy FindProtobuf.cmake to package
+        self.copy("*.cmake", dst=".", src="cmake/")
+        # Copy Headers to package include folder
+        self.copy_headers("*.h", "{}/src".format(self.folder))
+        # Copy all proto files:
+        self.copy("*.proto", dst="bin", src="{}/src".format(self.folder))
 
         if self.settings.os == "Windows":
-            self.copy("*.lib", "lib", "protobuf-3.1.0/cmake", keep_path=False)
-            self.copy("protoc.exe", "bin", "protobuf-3.1.0/cmake/%s" % self.settings.build_type, keep_path=False)
+            self.copy("*.lib", dst="lib", src="lib", keep_path=False)
+            self.copy("protoc.exe", dst="bin", src="bin", keep_path=False)
 
             if self.options.shared:
-                self.copy("*.dll", "bin", "protobuf-3.1.0/cmake/%s" % self.settings.build_type, keep_path=False)
+                self.copy("*.dll", dst="bin", src="", keep_path=False)
         else:
             # Copy the libs to lib
             if not self.options.shared:
-                self.copy("*.a", "lib", "protobuf-3.1.0/src/.libs", keep_path=False)
+                self.copy("*.a", "lib", "", keep_path=False)
             else:
-                self.copy("*.so*", "lib", "protobuf-3.1.0/src/.libs", keep_path=False)
-                self.copy("*.9.dylib", "lib", "protobuf-3.1.0/src/.libs", keep_path=False)
+                self.copy("*.so*", "lib", "", keep_path=False)
+                self.copy("*.9.dylib", "lib", "", keep_path=False)
 
             # Copy the exe to bin
-            if self.settings.os == "Macos":
-                if not self.options.shared:
-                    self.copy("protoc", "bin", "protobuf-3.1.0/src/", keep_path=False)
-                else:
-                    # "protoc" has libproto*.dylib dependencies with absolute file paths.
-                    # Change them to be relative.
-                    self.run("cd protobuf-3.1.0/src/.libs && bash ../../cmake/change_dylib_names.sh")
-
-                    # "src/protoc" may be a wrapper shell script which execute "src/.libs/protoc".
-                    # Copy "src/.libs/protoc" instead of "src/protoc"
-                    self.copy("protoc", "bin", "protobuf-3.1.0/src/.libs/", keep_path=False)
-                    self.copy("*.9.dylib", "bin", "protobuf-3.1.0/src/.libs", keep_path=False)
-            else:
-                self.copy("protoc", "bin", "protobuf-3.1.0/src/", keep_path=False)
+            # we need some sort of dynlib converter for macosx here, see memshardeds protobuf
+            self.copy("protoc", "bin", "bin", keep_path=False)
 
     def package_info(self):
         basename = "libprotobuf"
